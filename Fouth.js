777f325a34028b514d81598a3af4a09da3f9f2a4
Sonu9631 <142131522+Sonu9631@users.noreply.github.com>
2024-03-06 13:57:24 +0530

    Javascript file

---
+++ Fouth.js
@@ -0,0 +1,5 @@
+let numberOfChilds = 2;
+let numberOfAdults = 1;
+let numberOfSeniors = 1;
+let totalprice = (numberOfChilds*100+numberOfAdults*150+numberOfSeniors*120);
+console.log("The total ticket price is "+totalprice)
